99c75dafda34680529f45a0a46be8b28a31fb986
joaco <lemusmartinezanibaljoaquin@gmail.com>
2023-12-26 16:44:21 -0600

    HeaderLector implemented

--- src/HeaderLector.java
+++ src/HeaderLector.java
@@ -1,3 +1,36 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
+
 public class HeaderLector {
-    
+
+    private int fileSize;
+    private int width;
+    private int height;
+
+    public HeaderLector(File bmp) {
+        try (FileInputStream rawData = new FileInputStream(bmp)) {
+            byte[] header = rawData.readNBytes(54); // Header and InfoHeader bytes
+            fileSize = ((header[5] & 0xFF) << 24) | ((header[4] & 0xFF) << 16) | ((header[3] & 0xFF) << 8) | (header[2] & 0xFF);
+            width = ((header[21] & 0xFF) << 24) | ((header[20] & 0xFF) << 16) | ((header[19] & 0xFF) << 8) | (header[18] & 0xFF);
+            height = ((header[25] & 0xFF) << 24) | ((header[24] & 0xFF) << 16) | ((header[23] & 0xFF) << 8) | (header[22] & 0xFF);    
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public int getSize() {
+        return this.fileSize;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
 }
