53eb9217f87d618a7fa35ecf69bb23980581e9e7
joaco <lemusmartinezanibaljoaquin@gmail.com>
2023-12-26 20:11:43 -0600

    reason behind 54

--- src/HeaderLector.java
+++ src/HeaderLector.java
@@ -5,13 +5,15 @@ import java.io.File;
 
 public class HeaderLector {
 
+    private byte[] header;
     private int fileSize;
     private int width;
     private int height;
 
     public HeaderLector(File bmp) {
         try (FileInputStream rawData = new FileInputStream(bmp)) {
-            byte[] header = rawData.readNBytes(54); // Header and InfoHeader bytes
+            // lenght 54 = Header (14 bytes) + InfoHeader(40 bytes)
+            this.header = rawData.readNBytes(54); // Header and InfoHeader bytes
             fileSize = ((header[5] & 0xFF) << 24) | ((header[4] & 0xFF) << 16) | ((header[3] & 0xFF) << 8) | (header[2] & 0xFF);
             width = ((header[21] & 0xFF) << 24) | ((header[20] & 0xFF) << 16) | ((header[19] & 0xFF) << 8) | (header[18] & 0xFF);
             height = ((header[25] & 0xFF) << 24) | ((header[24] & 0xFF) << 16) | ((header[23] & 0xFF) << 8) | (header[22] & 0xFF);    
