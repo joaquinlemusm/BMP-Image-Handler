d25973779d24704cdd25138706be740160651f75
joaco <lemusmartinezanibaljoaquin@gmail.com>
2023-12-26 20:37:13 -0600

    set up of usage and -basics flag implemented

---
+++ src/BMPImageHandler.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+
+public class BMPImageHandler {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 2 || args.length < 2) {
+            System.out.println("Usage: java BMPImageHandler <flag> <file>");
+            return;
+        }
+        
+        String flag = args[0];
+        String file = args[1];
+
+        BmpHandlerCore basics = new BmpHandlerCore(file);
+
+        switch(flag){
+            case "-basics":
+                basics.coloredImages();
+                break;
+            case "-rotate":
+                System.out.println("Not implemented");
+                break;
+            case "-resize":
+                System.out.println("Not implemented");
+                break;
+            case "-all":
+                System.out.println("Not implemented");
+                break;  
+            default:
+                System.out.println("Invalid flag");
+                return;
+        }
+        System.out.println("Changes over the BMP file were successful.");
+    }
+} 
